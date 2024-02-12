68df7dbc90e12912331d277ea6389f07f4b6cedf
Pavel Krasovsky <frostcat2002@gmail.com>
2024-02-12 14:23:39 +0300

    Add: frontend base

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+
+export const Header = () => {
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>KRASOUSKI</div>
+          </Link>
+          <div className={styles.buttons}>
+            <Link to="/login">
+              <Button variant="outlined">Войти</Button>
+            </Link>
+            <Link to="/register">
+              <Button variant="contained">Создать аккаунт</Button>
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
